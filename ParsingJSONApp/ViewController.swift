5039c43f1a49cc5dbf1a82bd4645c45f8ee07f89
Alex Zhulai <Lepiozdyx@gmail.com>
2023-05-05 12:47:47 -0300

    Initial Commit

---
+++ ParsingJSONApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParsingJSONApp
+//
+//  Created by Alex on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
